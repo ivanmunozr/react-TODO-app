4e65afeab3e64f9e1c2e84b4217456fa7e886a05
Ivan Muñoz <ivan.munoz.r1@outlook.com>
2019-10-14 02:09:09 +0000

    funcionalidad de eliminar las tareas completa

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -10,11 +10,18 @@ export class Home extends React.Component {
 		super();
 		this.state = {
 			value: "",
-			tareas: ["No tasks, add a task"]
+			tareas: [
+				"No tasks, add a task",
+				"Tarea 1",
+				"Tarea 2",
+				"Tarea 3",
+				"Tarea 4"
+			]
 		};
 
 		this.controlaCambio = this.controlaCambio.bind(this);
 		this.controlaSubmit = this.controlaSubmit.bind(this);
+		this.eliminaTarea = this.eliminaTarea.bind(this);
 	}
 	controlaCambio(evento) {
 		this.setState({ value: evento.target.value });
@@ -25,19 +32,31 @@ export class Home extends React.Component {
 		nuevaTarea.push(this.state.value);
 		this.setState({ tareas: nuevaTarea });
 		evento.preventDefault();
+		this.setState({ value: "" });
+	}
+
+	eliminaTarea(index) {
+		let tareas = this.state.tareas.slice();
+		tareas.splice(index, 1);
+		this.setState({ tareas: tareas });
 	}
 
 	render() {
+		const contador = this.state.tareas.length;
+
 		return (
 			<div className="text-center mt-5">
 				<h1>todos</h1>
-				<p>{this.state.tareas}</p>
 				<Input
 					value={this.state.value}
 					controlCambio={this.controlaCambio}
 					controlSubmitt={this.controlaSubmit}
 				/>
-				<Lista />
+				<Lista
+					contador={contador}
+					lista={this.state.tareas}
+					elimina={this.eliminaTarea}
+				/>
 			</div>
 		);
 	}
--- src/js/component/listgroup.js
+++ src/js/component/listgroup.js
@@ -1,15 +1,31 @@
 import React from "react";
 
 export class Lista extends React.Component {
+	removeItem(index) {
+		this.props.elimina(index);
+	}
+
 	render() {
+		let listaTareas = this.props.lista.map((item, index) => (
+			<li className="list-group-item">
+				{index}
+				{item}
+				<button
+					type="button"
+					className="close"
+					aria-label="Close"
+					onClick={this.removeItem.bind(this, index)}>
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</li>
+		));
+
 		return (
 			<div className="card">
-				<ul className="list-group list-group-flush">
-					<li className="list-group-item">Cras justo odio</li>
-					<li className="list-group-item">Dapibus ac facilisis in</li>
-					<li className="list-group-item">Vestibulum at eros</li>
-				</ul>
-				<div className="card-footer text-muted">2 item left</div>
+				<ul className="list-group list-group-flush">{listaTareas}</ul>
+				<div className="card-footer text-muted">
+					{this.props.contador} item left
+				</div>
 			</div>
 		);
 	}
